bab87b0aae8290fc6d109687f0fc89729367bd63
KHermanUBB <kherman@ubiobio.cl>
2021-10-01 15:52:27 -0300

    lcd integrated

--- components/lcd/include/lcd.h
+++ components/lcd/include/lcd.h
@@ -7,8 +7,60 @@
  * COPYRIGHT NOTICE: (c) 2021  All rights reserved.
  */ 
 
+#include <esp_system.h>
+
 #ifndef LCD_H
 #define LCD_H
+
+// LCD module defines
+#define LCD_LINEONE             0x00        // start of line 1
+#define LCD_LINETWO             0x40        // start of line 2
+#define LCD_LINETHREE           0x14        // start of line 3
+#define LCD_LINEFOUR            0x54        // start of line 4
+
+#define LCD_BACKLIGHT           0x08
+#define LCD_ENABLE              0x04               
+#define LCD_COMMAND             0x00
+#define LCD_WRITE               0x01
+
+#define LCD_SET_DDRAM_ADDR      0x80
+#define LCD_READ_BF             0x40
+
+// LCD instructions
+#define LCD_CLEAR               0x01        // replace all characters with ASCII 'space'
+#define LCD_HOME                0x02        // return cursor to first position on first line
+#define LCD_ENTRY_MODE          0x06        // shift cursor from left to right on read/write
+#define LCD_DISPLAY_OFF         0x08        // turn display off
+#define LCD_DISPLAY_ON          0x0C        // display on, cursor off, don't blink character
+#define LCD_FUNCTION_RESET      0x30        // reset the LCD
+#define LCD_FUNCTION_SET_4BIT   0x38        // 4-bit data, 2-line display, 5 x 7 font
+#define LCD_SET_CURSOR          0x40        // set cursor position
+
+#define MAXIMA_LONGITUD         100
+
+
+#define LCD_addr  0x27
+#define SDA_pin   21
+#define SCL_pin   19
+#define LCD_cols  16
+#define LCD_rows 2
+
+// Pin mappings
+// P0 -> RS
+// P1 -> RW
+// P2 -> E
+// P3 -> Backlight
+// P4 -> D4
+// P5 -> D5
+// P6 -> D6
+// P7 -> D7
+
+
+static void LCD_writeNibble(uint8_t nibble, uint8_t mode);
+static void LCD_writeByte(uint8_t data, uint8_t mode);
+static void LCD_pulseEnable(uint8_t nibble);
+void LCD_init();
+
 
 
 #endif /* LCD_H */
